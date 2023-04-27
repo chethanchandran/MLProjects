cef6086c44d70ee2e3daaa980a0a8e172739c45a
Chethan Chandran <chethanchandran@Chethans-MBP.Dlink>
2023-04-27 08:29:22 +0530

    Model created

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Chethan',
+author_email='chandran.yr@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
